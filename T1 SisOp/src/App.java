328cecd539fda2e46b9e889667659c375d83ac00
Gustavo Pereira De Melo <Gustavo.Pereira96@edu.pucrs.br>
2024-09-12 19:59:18 -0300

    atualização das classes

--- T1 SisOp/src/App.java
+++ T1 SisOp/src/App.java
@@ -10,102 +10,14 @@ Gerencia a fila de processos e realiza o escalonamento.
 Classe Main
 
 Contém o método main para executar o programa e testar o escalonador.*/
-Código Esqueleto
-1. Classe Processo
-public class Processo {
-    private int id;
-    private int prioridade;
-    private int creditos;
-    private int tempoCPU;
-    private int tempoExecutado;
-    private boolean bloqueado;
-    private int ordem; // Utilizado para desempate
-
-    public Processo(int id, int prioridade, int tempoCPU, int ordem) {
-        this.id = id;
-        this.prioridade = prioridade;
-        this.creditos = prioridade;
-        this.tempoCPU = tempoCPU;
-        this.tempoExecutado = 0;
-        this.bloqueado = false;
-        this.ordem = ordem;
-    }
-
-    // Getters e setters
-
-    public void executar(int tempo) {
-        // Executa o processo por um determinado tempo
-        if (bloqueado) return;
-        tempoExecutado += tempo;
-        tempoCPU -= tempo;
-        if (tempoCPU <= 0) {
-            // Processo finalizado
-        }
-    }
-
-    public void bloquear() {
-        bloqueado = true;
-    }
-
-    public void liberar() {
-        bloqueado = false;
-    }
-
-    public void atualizarCreditos() {
-        creditos = (creditos / 2) + prioridade - 1;
-    }
-
-    // Outros métodos conforme necessário
-}
+//Código Esqueleto
 //2. Classe Escalonador
 import java.util.PriorityQueue;
 import java.util.Comparator;
 
-public class Escalonador {
-    private PriorityQueue<Processo> filaProntos;
-    private int tempoAtual;
 
-    public Escalonador() {
-        filaProntos = new PriorityQueue<>(Comparator.comparingInt(Processo::getCreditos).reversed().thenComparingInt(Processo::getOrdem));
-        tempoAtual = 0;
-    }
-
-    public void adicionarProcesso(Processo p) {
-        filaProntos.add(p);
-    }
-
-    public void escalonar() {
-        while (!filaProntos.isEmpty()) {
-            Processo processoAtual = filaProntos.poll();
-            if (processoAtual.getCreditos() > 0) {
-                processoAtual.executar(1); // Executa por 1ms
-                processoAtual.setCreditos(processoAtual.getCreditos() - 1);
-                
-                if (processoAtual.getCreditos() <= 0) {
-                    processoAtual.bloquear();
-                }
-                
-                if (processoAtual.getTempoCPU() > 0) {
-                    filaProntos.add(processoAtual);
-                }
-                
-                tempoAtual++;
-            } else {
-                atualizarCreditos();
-            }
-        }
-    }
-
-    private void atualizarCreditos() {
-        for (Processo p : filaProntos) {
-            p.atualizarCreditos();
-        }
-    }
-
-    // Outros métodos conforme necessário
-}
 //3. Classe Main
-public class Main {
+public class App {
     public static void main(String[] args) {
         Escalonador escalonador = new Escalonador();
         
---
+++ T1 SisOp/src/Processo.java
@@ -0,0 +1,45 @@
+public class Processo {
+    private int id;
+    private int prioridade;
+    private int creditos;
+    private int tempoCPU;
+    private int tempoExecutado;
+    private boolean bloqueado;
+    private int ordem; // Utilizado para desempate
+
+    public Processo(int id, int prioridade, int tempoCPU, int ordem) {
+        this.id = id;
+        this.prioridade = prioridade;
+        this.creditos = prioridade;
+        this.tempoCPU = tempoCPU;
+        this.tempoExecutado = 0;
+        this.bloqueado = false;
+        this.ordem = ordem;
+    }
+
+    // Getters e setters
+
+    public void executar(int tempo) {
+        // Executa o processo por um determinado tempo
+        if (bloqueado) return;
+        tempoExecutado += tempo;
+        tempoCPU -= tempo;
+        if (tempoCPU <= 0) {
+            // Processo finalizado
+        }
+    }
+
+    public void bloquear() {
+        bloqueado = true;
+    }
+
+    public void liberar() {
+        bloqueado = false;
+    }
+
+    public void atualizarCreditos() {
+        creditos = (creditos / 2) + prioridade - 1;
+    }
+
+    // Outros métodos conforme necessário
+}
